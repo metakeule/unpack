729c0e295c81667ed7cc8429b6d8c12475af198e
Marc Rene Arns <arns@stridor.net>
2016-08-01 17:02:12 +0200

    improve documentation for library

--- unpack.v1/unpack.go
+++ unpack.v1/unpack.go
@@ -14,16 +14,16 @@ func init() {
 	MustRegisterUnpacker(".rar", "unrar x [FILE]")
 	MustRegisterUnpacker(".7z", "7z x [FILE]")
 	MustRegisterUnpacker(".gz", "gzip -d [FILE]")
-
 }
 
-// registers the given cmd for the given extension. extension must start with '.'
-// cmd must contain [FILE] placeholder for filename
+// RegisterUnpacker registers the given cmd for the given extension ext.
+// ext must start with "." like e.g. ".zip"
+// cmd must contain [FILE] placeholder for filename, e.g. "unzip [FILE]"
 func RegisterUnpacker(ext string, cmd string) error {
 	return lib.RegisterUnpacker(ext, cmd)
 }
 
-// cmd must contain [FILE] placeholder for filename
+// MustRegisterUnpacker is like RegisterUnpacker but panicks if there is an error.
 func MustRegisterUnpacker(ext string, cmd string) {
 	err := RegisterUnpacker(ext, cmd)
 	if err != nil {
@@ -31,33 +31,44 @@ func MustRegisterUnpacker(ext string, cmd string) {
 	}
 }
 
-// RemoveArchive removes the archive file after successful extraction
+// RemoveArchive is an Option that removes the archive file after successful unpacking.
+// It is meant to be passed to New().
 var RemoveArchive Option = func(c *config) {
 	c.removeArchive = true
 }
 
-// RemoveDirectories removes typical directories to be removed within extracted files, like __MACOSX, .git and .svn
+// RemoveDirectories returns an Option that removes typical directories to be removed within extracted files, like __MACOSX, .git and .svn.
+// It is meant to be passed to New().
 func RemoveDirectories(dirs ...string) Option {
 	return func(c *config) {
 		c.rmDirs = dirs
 	}
 }
 
+// LogVerbose is an Option that enables verbose logging. This also includes error logging and info logging.
+// It is meant to be passed to New().
 var LogVerbose Option = func(c *config) {
 	c.logLevel = 2
 }
 
+// LogErrors is an Option that enables error logging.
+// It is meant to be passed to New().
 var LogErrors Option = func(c *config) {
 	c.logLevel = 0
 }
 
+// LogInfos is an Option that enables info logging. This also includes error logging.
+// It is meant to be passed to New().
 var LogInfos Option = func(c *config) {
 	c.logLevel = 1
 }
 
+// Option is a configuration option that is meant to be passed to New().
 type Option func(*config)
 
-// by default, no logging is enabled
+// New returns a new unpacker.
+// By default, logging is disabled. To enable it, pass one of the logging options as parameter.
+// New accepts options of type Option to enabled configuration.
 func New(opts ...Option) interface {
 	UnpackFile(string) error
 	UnpackAllFiles(string) map[string]error
@@ -79,11 +90,15 @@ type config struct {
 	logLevel      int
 }
 
-// file can be either relative to cwd of absolute path
-// the files are unpacked in to the newly created folder with the name of the archive file - extension
-// the archive file is also moved into this folder
+// UnpackFile unpacks the given file into a subdirectory which is named after the file (- its extension)
+// The subdirectory is created in the same folder where file resides.
+// Before unpacking, the file is moved to the subdirectory.
+// After uncompressing, the content of the subdirectory will be flattened by one level, i.e.
+// If there is just one subdirectory, its content is moved one level up.
 // it will also try to "flatten" the directory, i.e. if there is just one single folder in it
-// the content of this folder will be moved one folder up
+// the content of this folder will be moved one folder up.
+// If RemoveArchive was set, file is removed after successful unpacking.
+// Any directories set via RemoveDirectories will be removed inside the unpacked directory.
 func (c *config) UnpackFile(file string) (err error) {
 	file, err = filepath.Abs(file)
 	if err != nil {
@@ -92,22 +107,17 @@ func (c *config) UnpackFile(file string) (err error) {
 	return lib.UnpackFile(filepath.Base(file), filepath.Dir(file), c.removeArchive, c.rmDirs, c.logLevel)
 }
 
-// unpacks all files that have a known unpacker
-// dir can be either relative or absolute path
-// the files are unpacked in to the newly created folder with the name of the archive file - extension
-// the archive file is also moved into this folder
-// it will try to "flatten" the directory, i.e. if there is just one single folder in it
-// the content of this folder will be moved one folder up
+// UnpackAllFiles is like UnpackFile, but acting on all files with an extension for which a unpacker command
+// has been registered. By default that includes: ".tgz",".tar",".zip",".rar",".7z",".gz"
+// Make sure the corresponding command is available since otherwise in the middle of the processing there will
+// be a problem when the command is executed. If so that function returns at a state when the archive file has
+// been moved to the newly created folder (see documentation of UnpackFile).
 func (c *config) UnpackAllFiles(dir string) (errors map[string]error) {
 	return c.unpackFilesInDir(dir, fileHasUnpacker)
 }
 
-// unpacks all files which filename matches the pattern
-// dir can be either relative or absolute path
-// the files are unpacked in to the newly created folder with the name of the archive file - extension
-// the archive file is also moved into this folder
-// it will try to "flatten" the directory, i.e. if there is just one single folder in it
-// the content of this folder will be moved one folder up
+// UnpackFilesMatching is like UnpackAllFiles but only affects the files that are matching the given pattern.
+// The pattern must be a valid regular expression.
 func (c *config) UnpackFilesMatching(dir string, pattern string) (errors map[string]error) {
 	r, err := regexp.Compile(pattern)
 
